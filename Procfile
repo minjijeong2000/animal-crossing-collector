33e3b40012c27695946ee9b93417a966baddf788
Minji <dudvnd13@gmail.com>
2022-08-10 17:15:03 -0700

    Config deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn animal-crossing-collector.wsgi
--- animalcrossingcollector/settings.py
+++ animalcrossingcollector/settings.py
@@ -12,6 +12,16 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import environ
+env = environ.Env()
+environ.Env.read_env()
+import django_on_heroku
+
+django_on_heroku.settings(locals())
+
+DEBUG_PROPAGATE_EXCEPTIONS = True
+
+WSGI_APPLICATION = 'animalcrossingcollector.wsgi.application'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +31,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5q9+ok4ju!7m9uob=*f5=iv2mu&_3yfo+oq%q0k(2yk#hh-h*4'
-
+env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG') == 'True' 
 
 ALLOWED_HOSTS = []
 
